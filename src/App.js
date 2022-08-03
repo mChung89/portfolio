392f60bc09e6efba86f8542e1e23a3e824d24232
Michael Chung <mikechung89@gmail.com>
2022-08-03 00:54:50 -0400

    background style

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>My Personal Portfolio</h1>
+      <div className='background'>
+        <h1 style={{"color" : "white"}}>My Personal Portfolio</h1>
+      </div>
     </div>
   );
 }
